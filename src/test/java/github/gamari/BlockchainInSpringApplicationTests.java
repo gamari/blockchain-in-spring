3b8151d5f7ba94a87c28c9e06a11bfa2ceb13096
gamari <gamari4552@gmail.com>
2022-06-15 23:32:39 +0900

    init project.

---
+++ src/test/java/github/gamari/BlockchainInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package github.gamari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
